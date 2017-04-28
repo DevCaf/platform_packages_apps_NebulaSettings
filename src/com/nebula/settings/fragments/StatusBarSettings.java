8e0f34db5b6e708fd1ec27255e4923ccb46d011e
Eliminater74 <eliminater74@gmail.com>
2017-04-28 13:44:48 -0400

    NebulaSettings: Nebula initial Switch Over: Last Original Author: @spezi77 ** PixelDust Project **
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

---
+++ src/com/nebula/settings/fragments/StatusBarSettings.java
@@ -0,0 +1,49 @@
+package com.nebula.settings.fragments;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.UserHandle;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+
+import android.text.TextUtils;
+import android.view.View;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import android.util.Log;
+
+public class StatusBarSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.nebula_settings_statusbar);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.NEBULA;
+    }
+
+}
