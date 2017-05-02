pixeldustproject/android_packages_apps_PixelDustSettings

Make QS columns count configurable [2/2] Adjusted maxwen's commit found here http://gerrit.dirtyunicorns.com/#/c/2861/ so needed this commit to bring everything together
PureNexus edits:
minimum 3 tiles maximum 6

PD Edits:
qs_layout_columns slider: change max property to make it work

Signed-off-by: spezi77 <spezi77@gmx.com>

src/com/pixeldust/settings/fragments/QuickSettings.java
@@ -60,12 +60,15 @@ import com.android.settings.Utils;
 import java.util.List;
 import java.util.ArrayList;
 
+import com.pixeldust.settings.preferences.CustomSeekBarPreference;
+
 public class QuickSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
     private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
     private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
+    private static final String PREF_COLUMNS = "qs_layout_columns";
     private static final String DAYLIGHT_HEADER_PACK = "daylight_header_pack";
     private static final String DEFAULT_HEADER_PACKAGE = "com.android.systemui";
     private static final String CUSTOM_HEADER_IMAGE_SHADOW = "status_bar_custom_header_shadow";
@@ -78,6 +81,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private ListPreference mDaylightHeaderPack;
     private CustomSeekBarPreference mHeaderShadow;
     private ListPreference mHeaderProvider;
+    private CustomSeekBarPreference mQsColumns;
     private String mDaylightHeaderProvider;
     private PreferenceScreen mHeaderBrowse;
 
@@ -110,6 +114,12 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         mTileAnimationInterpolator.setValue(String.valueOf(tileAnimationInterpolator));
         updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
         mTileAnimationInterpolator.setOnPreferenceChangeListener(this);
+
+        mQsColumns = (CustomSeekBarPreference) findPreference(PREF_COLUMNS);
+        int columnsQs = Settings.System.getInt(resolver,
+                Settings.System.QS_LAYOUT_COLUMNS, 3);
+        mQsColumns.setValue(columnsQs / 1);
+        mQsColumns.setOnPreferenceChangeListener(this);
 
         String settingHeaderPackage = Settings.System.getString(resolver,
                 Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK);
@@ -184,6 +194,10 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             Settings.System.putIntForUser(getContentResolver(), Settings.System.ANIM_TILE_INTERPOLATOR,
                     tileAnimationInterpolator, UserHandle.USER_CURRENT);
             updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
+            return true;
+        } else if (preference == mQsColumns) {
+            int qsColumns = (Integer) newValue;
+            Settings.System.putInt(resolver, Settings.System.QS_LAYOUT_COLUMNS, qsColumns * 1);
             return true;
         } else if (preference == mDaylightHeaderPack) {
             String value = (String) newValue;
@@ -283,5 +297,4 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     protected int getMetricsCategory() {
         return MetricsEvent.PIXELDUST;
     }
-
 }

DevCaf/platform_packages_apps_NebulaSettings

Signed-off-by: Eliminater74 <eliminater74@gmail.com>

src/com/nebula/settings/fragments/QuickSettings.java
@@ -60,12 +60,15 @@ import com.android.settings.Utils;
 import java.util.List;
 import java.util.ArrayList;
 
+import com.nebula.settings.preferences.CustomSeekBarPreference;
+
 public class QuickSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
     private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
     private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
+    private static final String PREF_COLUMNS = "qs_layout_columns";
     private static final String DAYLIGHT_HEADER_PACK = "daylight_header_pack";
     private static final String DEFAULT_HEADER_PACKAGE = "com.android.systemui";
     private static final String CUSTOM_HEADER_IMAGE_SHADOW = "status_bar_custom_header_shadow";
@@ -78,6 +81,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private ListPreference mDaylightHeaderPack;
     private CustomSeekBarPreference mHeaderShadow;
     private ListPreference mHeaderProvider;
+    private CustomSeekBarPreference mQsColumns;
     private String mDaylightHeaderProvider;
     private PreferenceScreen mHeaderBrowse;
 
@@ -110,6 +114,12 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         mTileAnimationInterpolator.setValue(String.valueOf(tileAnimationInterpolator));
         updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
         mTileAnimationInterpolator.setOnPreferenceChangeListener(this);
+
+        mQsColumns = (CustomSeekBarPreference) findPreference(PREF_COLUMNS);
+        int columnsQs = Settings.System.getInt(resolver,
+                Settings.System.QS_LAYOUT_COLUMNS, 3);
+        mQsColumns.setValue(columnsQs / 1);
+        mQsColumns.setOnPreferenceChangeListener(this);
 
         String settingHeaderPackage = Settings.System.getString(resolver,
                 Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK);
@@ -184,6 +194,10 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             Settings.System.putIntForUser(getContentResolver(), Settings.System.ANIM_TILE_INTERPOLATOR,
                     tileAnimationInterpolator, UserHandle.USER_CURRENT);
             updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
+            return true;
+        } else if (preference == mQsColumns) {
+            int qsColumns = (Integer) newValue;
+            Settings.System.putInt(resolver, Settings.System.QS_LAYOUT_COLUMNS, qsColumns * 1);
             return true;
         } else if (preference == mDaylightHeaderPack) {
             String value = (String) newValue;
@@ -283,5 +297,4 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     protected int getMetricsCategory() {
         return MetricsEvent.NEBULA;
     }
-
 }