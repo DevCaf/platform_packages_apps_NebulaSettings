pixeldustproject/android_packages_apps_PixelDustSettings

incall vibration options [3/3]
* taken from PureNexus

Signed-off-by: spezi77 <spezi77@gmx.com>

src/com/pixeldust/settings/fragments/MiscSettings.java
@@ -24,12 +24,15 @@ import com.android.internal.util.pixeldust.ActionUtils;
 import com.android.settings.DevelopmentSettings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.pixeldust.settings.utils.Utils;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
 public class MiscSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
 
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
@@ -47,6 +50,11 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.pixeldust_settings_misc);
         PreferenceScreen prefs = getPreferenceScreen();
+
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            prefs.removePreference(incallVibCategory);
+        }
 
         mScrollingCachePref = (ListPreference) findPreference(SCROLLINGCACHE_PREF);
         mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,

DevCaf/platform_packages_apps_NebulaSettings

Signed-off-by: Eliminater74 <eliminater74@gmail.com>

src/com/nebula/settings/fragments/MiscSettings.java
@@ -24,6 +24,7 @@ import com.android.internal.util.nebula.ActionUtils;
 import com.android.settings.DevelopmentSettings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.nebula.settings.utils.Utils;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -48,6 +49,11 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.nebula_settings_misc);
         PreferenceScreen prefs = getPreferenceScreen();
+
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            prefs.removePreference(incallVibCategory);
+        }
 
         mScrollingCachePref = (ListPreference) findPreference(SCROLLINGCACHE_PREF);
         mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,